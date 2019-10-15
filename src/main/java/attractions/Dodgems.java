b483c187bbd8ae49d11aeac1d66cdc34c1ae61e4
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-10-15 21:24:14 +0100

    added method and tests for dodgems

--- src/main/java/attractions/Dodgems.java
+++ src/main/java/attractions/Dodgems.java
@@ -1,12 +1,23 @@
 package attractions;
 
+
+import behaviours.ITicketed;
 import people.Visitor;
 
-public class Dodgems extends Attraction {
+public class Dodgems extends Attraction implements ITicketed {
 
     public Dodgems(String name, int rating) {
         super(name, rating);
     }
 
 
+    public double defaultPrice() {
+        return 4.50;
+    }
+
+    public double priceFor(Visitor visitor) {
+        if(visitor.getAge() <12) {
+            return defaultPrice() / 2;
+        }return defaultPrice();
+    }
 }
--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,16 +2,21 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor ahmed;
+    Visitor amanda;
 
     @Before
     public void setUp() throws Exception {
         dodgems = new Dodgems("Bumper Cars", 5);
+        ahmed = new Visitor(11, 130, 8);
+        amanda = new Visitor(26, 165, 35);
     }
 
 
@@ -28,5 +33,15 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void payHalfPrice(){
+        assertEquals(2.25, dodgems.priceFor(ahmed), 0.01);
+    }
+
+    @Test
+    public void payDefaultPrice(){
+        assertEquals(4.50, dodgems.priceFor(amanda), 0.01);
     }
 }
