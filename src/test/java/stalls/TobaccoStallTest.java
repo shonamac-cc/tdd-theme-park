34baef35d9875137b8e74eb4f7c8463307a306bf
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-10-15 20:55:02 +0100

    added tobacco stall test for age

--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -2,16 +2,21 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
+    Visitor visitor;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        visitor = new Visitor(17, 165, 30);
+        visitor2 = new Visitor(25, 190, 28);
     }
 
     @Test
@@ -27,5 +32,15 @@ public class TobaccoStallTest {
     @Test
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void isAllowedToVisit(){
+        assertTrue(tobaccoStall.isAllowedTo(visitor2));
+    }
+
+    @Test
+    public void isNotAllowedToVisit(){
+        assertFalse(tobaccoStall.isAllowedTo(visitor));
     }
 }
